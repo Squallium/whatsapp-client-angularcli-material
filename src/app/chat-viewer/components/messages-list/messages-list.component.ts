Urigo/WhatsApp-Clone-Client-Angular

Step 7.2: Remove messages and chats

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {GetChat} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-messages-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let message of messages">
-        <app-message-item [item]="message" [isGroup]="isGroup"></app-message-item>
+        <app-message-item [item]="message" [isGroup]="isGroup"
+                          appSelectableItem></app-message-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['messages-list.component.scss'],
 })
@@ -20,5 +23,5 @@ export class MessagesListComponent {
   @Input()
   isGroup: boolean;
 
-  constructor() {}
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -12,7 +12,10 @@ import {GetChat} from '../../../../types';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         appSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -46,5 +49,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,14 +1,17 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GetChats} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-chats-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let chat of chats">
-        <app-chat-item [item]="chat" (select)="selectChat($event)"></app-chat-item>
+        <app-chat-item [item]="chat"
+                       appSelectableItem></app-chat-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['chats-list.component.scss'],
 })
@@ -17,12 +20,5 @@ export class ChatsListComponent {
   @Input('items')
   chats: GetChats.Chats[];
 
-  @Output()
-  select = new EventEmitter<string>();
-
-  constructor() {}
-
-  selectChat(id: string) {
-    this.select.emit(id);
-  }
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

Squallium/whatsapp-client-angularcli-material

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {GetChat} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-messages-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let message of messages">
-        <app-message-item [item]="message" [isGroup]="isGroup"></app-message-item>
+        <app-message-item [item]="message" [isGroup]="isGroup"
+                          libSelectableItem></app-message-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['messages-list.component.scss'],
 })
@@ -20,5 +23,5 @@ export class MessagesListComponent {
   @Input()
   isGroup: boolean;
 
-  constructor() {}
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -12,7 +12,10 @@ import {GetChat} from '../../../../types';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         libSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -46,5 +49,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,14 +1,17 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GetChats} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-chats-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let chat of chats">
-        <app-chat-item [item]="chat" (select)="selectChat($event)"></app-chat-item>
+        <app-chat-item [item]="chat"
+                       libSelectableItem></app-chat-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['chats-list.component.scss'],
 })
@@ -17,12 +20,5 @@ export class ChatsListComponent {
   @Input('items')
   chats: GetChats.Chats[];
 
-  @Output()
-  select = new EventEmitter<string>();
-
-  constructor() {}
-
-  selectChat(id: string) {
-    this.select.emit(id);
-  }
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }